f02e7cf96c07cabb01300b16c1b88ba8c34f71dd
Alessandro Cifani <alessandro.cifani@adevinta.com>
2023-03-22 15:45:38 +0100

    perf: try dynamic importing heavy components

--- app/message.tsx
+++ app/message.tsx
@@ -1,7 +1,13 @@
-import ReactMarkdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import dynamic from 'next/dynamic';
+import { Suspense, lazy } from 'react';
 import { oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import styles from './message.module.css';
+
+const SyntaxHighlighter = dynamic(() =>
+  import('react-syntax-highlighter').then((m) => m.Prism),
+);
+
+const ReactMarkdown = lazy(() => import('react-markdown'));
 
 export interface Message {
   role: 'assistant' | 'user' | 'system';
@@ -15,31 +21,35 @@ export function ChatLine({ msg }: { msg: Message }) {
     <div className={styles.message + ' ' + styles[msg.role]}>
       <p className={styles.role}>{msg.role}</p>
       <div>
-        <ReactMarkdown
-          components={{
-            code({ node, inline, className, children, ...props }) {
-              if (inline) {
-                return <code>{children}</code>;
-              }
+        <Suspense>
+          <ReactMarkdown
+            components={{
+              code({ node, inline, className, children, ...props }) {
+                if (inline) {
+                  return <code>{children}</code>;
+                }
 
-              const match = /language-(\w+)/.exec(className || '');
+                const match = /language-(\w+)/.exec(className || '');
 
-              return (
-                <SyntaxHighlighter
-                  // @ts-expect-error
-                  style={oneLight}
-                  language={match?.[1] || undefined}
-                  PreTag="div"
-                  {...props}
-                >
-                  {String(children).replace(/\n$/, '')}
-                </SyntaxHighlighter>
-              );
-            },
-          }}
-        >
-          {msg.content}
-        </ReactMarkdown>
+                return (
+                  <Suspense>
+                    <SyntaxHighlighter
+                      // @ts-expect-error
+                      style={oneLight}
+                      language={match?.[1] || undefined}
+                      PreTag="div"
+                      {...props}
+                    >
+                      {String(children).replace(/\n$/, '')}
+                    </SyntaxHighlighter>
+                  </Suspense>
+                );
+              },
+            }}
+          >
+            {msg.content}
+          </ReactMarkdown>
+        </Suspense>
       </div>
     </div>
   );
